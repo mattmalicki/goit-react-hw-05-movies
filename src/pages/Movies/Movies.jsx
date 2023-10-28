0481884115b8138c5a8f43227a1539091463f2b1
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 15:33:54 +0200

    Fetching search movies from API finished

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,19 +1,35 @@
 import { SearchForm } from 'components/atoms/SearchForm/SearchForm';
 import { MovieList } from 'components/molecules/MovieList/MovieList';
+import { useState, useEffect } from 'react';
+
+import { fetchMoviesWithName } from 'services/fetchFromApi';
 
 export const Movies = () => {
-  const array = [
-    { name: 'Test' },
-    { name: 'Test' },
-    { name: 'Test' },
-    { name: 'Test' },
-    { name: 'Test' },
-    { name: 'Test' },
-  ];
+  const [moviesArray, setMoviesArray] = useState([]);
+  const [querry, setQuerry] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    setQuerry(form.querry.value);
+    form.reset();
+  };
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const movies = await fetchMoviesWithName(querry, 1);
+        setMoviesArray([...movies.results]);
+      } catch (err) {
+        console.log('Error: ', err.toString());
+      }
+    }
+    getMovies();
+  }, [querry]);
   return (
     <>
-      <SearchForm />
-      <MovieList movies={array} />
+      <SearchForm onSubmit={handleSubmit} />
+      {moviesArray.length > 0 && <MovieList movies={moviesArray} />}
     </>
   );
 };
