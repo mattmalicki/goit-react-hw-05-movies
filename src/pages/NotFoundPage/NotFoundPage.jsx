661636d5e948b8bc2054ccde1a9bc22b799dc827
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 12:52:29 +0200

    Added NotFoundPage page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import styles from './NotFoundPage.module.css';
+
+export const NotFoundPage = () => {
+  return (
+    <div className={styles.NotFoundPage}>
+      <span>Page not found.</span>
+    </div>
+  );
+};
