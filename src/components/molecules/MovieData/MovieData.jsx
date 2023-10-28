8cb018216f4e6c0bd47aeff7b5dea9622000d648
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 18:16:29 +0200

    Styling finished

--- src/components/molecules/MovieData/MovieData.jsx
+++ src/components/molecules/MovieData/MovieData.jsx
@@ -1,14 +1,13 @@
 import styles from './MovieData.module.css';
 
 export const MovieData = ({ movie }) => {
+  const image = movie.poster_path
+    ? `https://image.tmdb.org/t/p/original${movie.poster_path}`
+    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtNlCvCIk-K3ZPzfPDDaLDK9GUr8aEOitcbQ&usqp=CAU';
   return (
     <div className={styles.Container}>
       <div className={styles.Image}>
-        <img
-          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-          width="100%"
-          alt={movie.title}
-        />
+        <img src={image} width="100%" alt={movie.title} />
       </div>
       <div className={styles.Info}>
         <div className={styles.Div}>
