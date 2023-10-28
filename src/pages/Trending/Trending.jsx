2f58a0fdbda29747ae97bb3f32b10e6199bc8154
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 15:10:49 +0200

    Adding functions to fetch from api

--- src/pages/Trending/Trending.jsx
+++ src/pages/Trending/Trending.jsx
@@ -1,6 +1,12 @@
+import { useEffect, useState, useRef } from 'react';
+
 import { MovieList } from 'components/molecules/MovieList/MovieList';
 
+import { fetchTrendingMovies } from 'services/fetchFromApi';
+
 export const Trending = () => {
+  const moviesArray = useRef([]);
+
   const array = [
     { name: 'Test' },
     { name: 'Test' },
@@ -9,6 +15,18 @@ export const Trending = () => {
     { name: 'Test' },
     { name: 'Test' },
   ];
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const movies = await fetchTrendingMovies();
+        moviesArray.current = movies.result;
+      } catch (err) {
+        console.log('Error: ', err.toString());
+      }
+    }
+    getMovies();
+  }, []);
   return (
     <>
       <h2>Trending today</h2>
