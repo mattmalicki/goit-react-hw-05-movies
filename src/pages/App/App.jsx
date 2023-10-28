0ec38b5e306aff9ebf9fb6690acdd38a22d795be
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 14:33:09 +0200

    Replacing MovieData code to MovieInfo and changing its path

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router';
 import { Movies } from 'pages/Movies/Movies';
 import { Header } from 'components/organisms/Header/Header';
 import { NotFoundPage } from 'pages/NotFoundPage/NotFoundPage';
-import { Trending } from 'components/organisms/Trending/Trending';
-import { MovieCard } from 'components/organisms/MovieCard/MovieCard';
+import { Trending } from 'pages/Trending/Trending';
+import { MovieInfo } from 'components/organisms/MovieCard/MovieInfo';
 
 export const App = () => {
   return (
@@ -13,7 +13,7 @@ export const App = () => {
         <Header />
         <Route path="/" element={<Trending />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieCard />} />
+        <Route path="/movies/:id" element={<MovieInfo />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
