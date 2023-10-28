0da11e6d0041200e0dff1d0a9e68c1c704f25489
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 14:23:04 +0200

    Adding route path to MovieCard

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,6 +4,7 @@ import { Movies } from 'pages/Movies/Movies';
 import { Header } from 'components/organisms/Header/Header';
 import { NotFoundPage } from 'pages/NotFoundPage/NotFoundPage';
 import { Trending } from 'components/organisms/Trending/Trending';
+import { MovieCard } from 'components/organisms/MovieCard/MovieCard';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
         <Header />
         <Route path="/" element={<Trending />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieCard />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
