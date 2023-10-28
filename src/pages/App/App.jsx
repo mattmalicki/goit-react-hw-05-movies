93676b7f74117fc94090a1a4330ed9f102c24b12
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-28 13:02:26 +0200

    Adding Routes and Header

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,16 +1,19 @@
+import { Routes, Route } from 'react-router';
+
+import { Movies } from 'pages/Movies/Movies';
+import { Header } from 'components/organisms/Header/Header';
+import { NotFoundPage } from 'pages/NotFoundPage/NotFoundPage';
+import { Trending } from 'components/organisms/Trending/Trending';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Header />
+        <Route path="/" element={<Trending />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
